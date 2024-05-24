cb8bcbbd3c082d0db91eb301ecaa38cad3d58a94
tmlcs <150374863+tmlcs@users.noreply.github.com>
2024-05-24 13:34:15 +0200

    Delete TMLCS_Proyect_Beta_2 directory

--- TMLCS_Proyect_Beta_2/FindCryptopp.cmake
+++
@@ -1,6 +0,0 @@
-# FindCryptopp.cmake
-find_path(Cryptopp_INCLUDE_DIR NAMES cryptopp/cryptlib.h PATH_SUFFIXES cryptopp)
-find_library(Cryptopp_LIBRARY NAMES cryptopp)
-
-set(Cryptopp_INCLUDE_DIRS ${Cryptopp_INCLUDE_DIR})
-set(Cryptopp_LIBRARIES ${Cryptopp_LIBRARY})
